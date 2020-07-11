414333bc23191dfd0e54ca73b736a21831974bc7
Macky <makrand586@gmail.com>
2020-07-11 18:59:47 +0530

    Updates

---
+++ precache-manifest.26ed0476bdd3dc52d487c932cf6cc733.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28b24f660f1b34cb0535348df93ac414",
+    "url": "/MyPortfolio/index.html"
+  },
+  {
+    "revision": "8cca08fd053cc30380cc",
+    "url": "/MyPortfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "baf07877a9268a990407",
+    "url": "/MyPortfolio/static/css/main.26e68399.chunk.css"
+  },
+  {
+    "revision": "8cca08fd053cc30380cc",
+    "url": "/MyPortfolio/static/js/2.b8256350.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/MyPortfolio/static/js/2.b8256350.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baf07877a9268a990407",
+    "url": "/MyPortfolio/static/js/main.d6c3cad9.chunk.js"
+  },
+  {
+    "revision": "93c65b4db3af1fa805d2",
+    "url": "/MyPortfolio/static/js/runtime-main.bd9119cd.js"
+  },
+  {
+    "revision": "d3e30c393c37d05e2a80a35b05b5a8d1",
+    "url": "/MyPortfolio/static/media/YanoneKaffeesatz-VariableFont_wght.d3e30c39.ttf"
+  },
+  {
+    "revision": "9adc98bd4bc6174ec685c7486973f764",
+    "url": "/MyPortfolio/static/media/mak.9adc98bd.jpg"
+  }
+]);
